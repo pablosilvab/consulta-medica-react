403f58a9c6eb3b9d983ab77548a2458b769f4e85
Pablo Silva <pablonicolassilvabravo@gmail.com>
2020-06-10 11:37:37 -0400

    WIP: Eliminar citas

--- src/App.js
+++ src/App.js
@@ -12,6 +12,14 @@ function App() {
     guardarCitas([...citas, cita]);
   };
 
+  // Eliminar cita
+  const eliminarCita = (id) => {
+    const nuevasCitas = citas.filter(cita => cita.id !== id );
+    guardarCitas(nuevasCitas);
+  }
+
+  // Condición
+  const titulo = (citas.length === 0 ) ? 'No hay citas' : 'Administra tus citas';
   
 
   return (
@@ -24,11 +32,12 @@ function App() {
           </div>
 
           <div className="one-half column">
-            <h2>Administrar citas</h2>
+            <h2>{titulo}</h2>
             {citas.map(cita =>(
               <Cita
                 key={cita.id}
                 cita={cita}
+                eliminarCita={eliminarCita}
               />  
             ))}
           </div>
--- src/components/Cita.js
+++ src/components/Cita.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
-const Cita = ({cita}) => (
+const Cita = ({cita, eliminarCita}) => (
     <div className="cita">
         <p>Paciente: <span>{cita.paciente}</span></p>
         <p>Previsión: <span>{cita.prevision}</span></p>
         <p>Fecha: <span>{cita.fecha}</span></p>
         <p>Hora: <span>{cita.hora}</span></p>
         <p>Síntomas: <span>{cita.sintomas}</span></p>
-
+        <button
+          className="button eliminar u-full-width"
+          onClick={() => eliminarCita(cita.id)}
+        >
+          Eliminar &times;
+        </button>
     </div>
   );
 
