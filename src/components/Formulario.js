31fbd685755dafb197e817fbdb3d61630e04b8ef
Pablo Silva <pablonicolassilvabravo@gmail.com>
2020-06-09 23:10:06 -0400

    WIP: Creación de state para citas - Leer lo que se escribe en los inputs

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,26 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const Formulario = () => {
+  // Crear state de citas médicas
+  const [cita, actualizarCita] = useState({
+    paciente: "",
+    prevision: "",
+    fecha: "",
+    hora: "",
+    sintomas: "",
+  });
+
+  // Fn que se ejecuta cuando el usuario escribe en un input -> actualizar state
+  const handleChange = e => {
+    actualizarCita({
+      ...cita, 
+      [e.target.name]: e.target.value
+    })
+  };
+
+  // Extraer valores
+  const { paciente, prevision, fecha, hora, sintomas} = cita;
+
   return (
     <Fragment>
       <h2>Crear cita</h2>
@@ -11,22 +31,46 @@ const Formulario = () => {
           name="paciente"
           className="u-full-width"
           placeholder="Nombre paciente"
+          onChange={handleChange}
+          value={paciente}
         />
+
         <label>Previsión</label>
         <input
           type="text"
           name="prevision"
           className="u-full-width"
           placeholder="Previsión"
+          onChange={handleChange}
+          value={prevision}
         />
+
         <label>Fecha</label>
-        <input type="date" name="fecha" className="u-full-width" />
+        <input
+          type="date"
+          name="fecha"
+          className="u-full-width"
+          onChange={handleChange}
+          value={fecha}
+        />
 
         <label>Hora</label>
-        <input type="time" name="hora" className="u-full-width" />
+        <input
+          type="time"
+          name="hora"
+          className="u-full-width"
+          onChange={handleChange}
+          value={hora}
+        />
 
         <label>Síntomas</label>
-        <textarea className="u-full-width" name="sintomas"></textarea>
+        <textarea
+          className="u-full-width"
+          name="sintomas"
+          onChange={handleChange}
+          value={sintomas}
+        ></textarea>
+
         <button type="submit" className="u-full-width button-primary">
           Agregar cita
         </button>
